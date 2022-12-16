158e73ba65078f4d21928af91073d63de0afd187
AndreaPanzerDev <andpanz@libero.it>
2022-12-16 17:16:35 +0100

    auto-save

--- index.php
+++ index.php
@@ -2,14 +2,8 @@
 
 include_once __DIR__ . '/classi/class.php';
 
-if (!empty($_GET) && !empty($_GET['name']) && !empty($_GET['genre']) && !empty($_GET['age'])) {
-    $name = $_GET['name'];
-    $genre = $_GET['genre'];
-    $age = $_GET['age'];
 
-    $movie = new Movies($name, $age, $genre);
-    var_dump($movie);
-}
+$salviamoidati = [];
 
 
 ?>
@@ -31,6 +25,7 @@ if (!empty($_GET) && !empty($_GET['name']) && !empty($_GET['genre']) && !empty($
         <h2>Inserisci le informazioni del film</h2>
     </header>
     <main class="text-center">
+
         <form method="GET">
             <input type="text" placeholder="Inserisci il nome del film" name="name">
             <input type="text" placeholder="Inserisci il genere del film" name="genre">
@@ -38,10 +33,25 @@ if (!empty($_GET) && !empty($_GET['name']) && !empty($_GET['genre']) && !empty($
             <button>Vai</button>
         </form>
 
-
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     </main>
 </body>
 
 </html>
+
+<?php
+
+
+
+
+if (!empty($_GET) && !empty($_GET['name']) && !empty($_GET['genre']) && !empty($_GET['age'])) {
+    $name = $_GET['name'];
+    $genre = $_GET['genre'];
+    $age = $_GET['age'];
+
+    $movie = new Movies($name, $age, $genre);
+    var_dump($movie);
+    $salviamoidati_push($movie);
+    var_dump($salviamoidati);
+}
 
